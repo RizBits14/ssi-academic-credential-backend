0e4c889596a28c786468b537632279adfb8faa8a
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2026-09-14 00:22:41 +0600

    chore: add environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 
 void bootstrap();
